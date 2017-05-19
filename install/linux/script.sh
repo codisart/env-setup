ff22c50ed60183f221ed4adccb1467dcba0817fa
codisart <louis.celier@gmail.com>
2017-05-19 15:43:27 +0200

    Replace submodule by simple clone at install for zend-validator and zend-hydrator

--- install/linux/script.sh
+++ install/linux/script.sh
@@ -1,5 +1,9 @@
 cd datasionning
 git clone https://github.com/codisart/datasioning.git repository
+cd ..
+
+git clone https://github.com/zendframework/zend-validator.git zend-validator/repository
+git clone https://github.com/zendframework/zend-hydrator.git zend-hydrator/repository
 
 
 
--- install/windows/script.bat
+++ install/windows/script.bat
@@ -1,1 +1,5 @@
+
+git clone https://github.com/zendframework/zend-validator.git zend-validator/repository
+git clone https://github.com/zendframework/zend-hydrator.git zend-hydrator/repository
+
 git config --local include.path ..\.gitconfig
