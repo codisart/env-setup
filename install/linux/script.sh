6bb1fec88f11a92adc690b33f845e1c8ed81c095
codisart <louis.celier@gmail.com>
2017-05-19 18:43:04 +0200

    Replace submodule for zend-file

--- install/linux/script.sh
+++ install/linux/script.sh
@@ -4,6 +4,7 @@ cd ..
 
 git clone https://github.com/zendframework/zend-validator.git zend-validator/repository
 git clone https://github.com/zendframework/zend-hydrator.git zend-hydrator/repository
+git clone https://github.com/zendframework/zend-file.git zend-file/repository
 
 
 
--- install/windows/script.bat
+++ install/windows/script.bat
@@ -1,5 +1,6 @@
 
 git clone https://github.com/zendframework/zend-validator.git zend-validator/repository
 git clone https://github.com/zendframework/zend-hydrator.git zend-hydrator/repository
+git clone https://github.com/zendframework/zend-file.git zend-file/repository
 
 git config --local include.path ..\.gitconfig
