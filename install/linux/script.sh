1853e531080aec37ce76e693fbdd4a4386d98f1a
codisart <louis.celier@gmail.com>
2018-01-18 14:00:38 +0100

    Add the nightwatch project

--- install/linux/script.sh
+++ install/linux/script.sh
@@ -5,6 +5,7 @@ cd ..
 git clone https://github.com/zendframework/zend-validator.git zend-validator/repository
 git clone https://github.com/zendframework/zend-hydrator.git zend-hydrator/repository
 git clone https://github.com/zendframework/zend-file.git zend-file/repository
+git clone https://github.com/zendframework/zend-file.git nightwash/repository
 
 
 
