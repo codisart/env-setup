213bf4720c9b44149cdb0376b60f61a3d0489e54
codisart <louis.celier@gmail.com>
2019-11-21 16:52:04 +0100

    dev(install): fix install scripts
    Rename datasioning folder

--- .install/windows/script.bat
+++ .install/windows/script.bat
@@ -1,7 +1,22 @@
+
+cd %~dp0
 cd ../..
 
-git clone https://github.com/zendframework/zend-validator.git zend-validator/repository
-git clone https://github.com/zendframework/zend-hydrator.git zend-hydrator/repository
-git clone https://github.com/zendframework/zend-file.git zend-file/repository
+git clone https://github.com/codisart/datasioning.git datasioning/repository
 
-git config --local include.path ..\.gitconfig
+git clone https://github.com/codisart/zend-validator.git zend-validator/repository
+cd zend-validator/repository
+git remote add upstream https://github.com/zendframework/zend-validator.git
+cd -
+
+git clone https://github.com/codisart/zend-hydrator.git zend-hydrator/repository
+cd zend-hydrator/repository
+git remote add upstream https://github.com/zendframework/zend-hydrator.git
+cd -
+
+git clone https://github.com/codisart/zend-file.git zend-file/repository
+cd zend-file/repository
+git remote add upstream https://github.com/zendframework/zend-file.git
+cd -
+
+git config --local include.path ../.gitconfig
