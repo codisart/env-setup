a940a0564d000fa5adf49c8f17df30534ff9328f
codisart <louis.celier@gmail.com>
2019-12-06 16:19:56 +0100

    Add two new projects

--- .install/linux/script.sh
+++ .install/linux/script.sh
@@ -6,6 +6,8 @@ git clone https://github.com/codisart/datasioning.git datasioning/repository
 
 git clone https://github.com/codisart/polygones.git polygones/repository
 
+git clone https://github.com/theloulous/php-mise-en-place.git mise-en-place/repository
+
 git clone https://github.com/codisart/zend-validator.git zend-validator/repository
 cd zend-validator/repository
 git remote add upstream https://github.com/zendframework/zend-validator.git
@@ -19,6 +21,11 @@ cd -
 git clone https://github.com/codisart/zend-file.git zend-file/repository
 cd zend-file/repository
 git remote add upstream https://github.com/zendframework/zend-file.git
+cd -
+
+git clone https://github.com/codisart/core.git owncloud/repository
+cd owncloud/repository
+git remote add upstream https://github.com/owncloud/core.git
 cd -
 
 git config --local include.path ../.gitconfig
