f155f982149d12029e9c4d3e393736747c69168e
codisart <louis.celier@gmail.com>
2020-02-11 18:45:44 +0100

    Replace all zend repos by laminas

--- .install/linux/script.sh
+++ .install/linux/script.sh
@@ -11,19 +11,14 @@ git clone https://github.com/codisart/polygones.git polygones/repository
 
 git clone https://github.com/codisart/storal-repository.git storal-repository/repository
 
-git clone https://github.com/codisart/zend-validator.git zend-validator/repository
-cd zend-validator/repository
-git remote add upstream https://github.com/zendframework/zend-validator.git
+git clone https://github.com/codisart/laminas-hydrator.git laminas-hydrator/repository
+cd laminas-hydrator/repository
+git remote add upstream https://github.com/laminas/laminas-hydrator.git
 cd -
 
-git clone https://github.com/codisart/zend-hydrator.git zend-hydrator/repository
-cd zend-hydrator/repository
-git remote add upstream https://github.com/zendframework/zend-hydrator.git
-cd -
-
-git clone https://github.com/codisart/zend-file.git zend-file/repository
-cd zend-file/repository
-git remote add upstream https://github.com/zendframework/zend-file.git
+git clone https://github.com/codisart/laminas-validator.git laminas-validator/repository
+cd laminas-validator/repository
+git remote add upstream https://github.com/laminas/laminas-validator.git
 cd -
 
 git clone https://github.com/codisart/core.git owncloud/repository
